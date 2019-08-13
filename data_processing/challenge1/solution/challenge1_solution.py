3cb714c457f3f21fb5da359feeb3099bf4841b92
TheChanRProject <garpchoo@gmail.com>
2019-08-12 17:10:30 -0700

    Adding edits

--- data_processing/challenge1/solution/challenge1_solution.py
+++ data_processing/challenge1/solution/challenge1_solution.py
@@ -1,3 +1,12 @@
+from os import getcwd, chdir
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
+chdir("data_processing/challenge1/")
+
+df = pd.read_csv("challenge1.csv")
+
+df.head()
+
+ 
